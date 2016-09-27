eba8103278c5f3abb7e7d3e4d411a0659e3abdc9
Hirams77 <15818456427@163.com>
2016-09-27 14:28:25 +0800

    这是用户中心的文件！

---
+++ app/src/main/java/com/administrator/mytestgitapplication/UserCenterActivity.java
@@ -0,0 +1,18 @@
+package com.administrator.mytestgitapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 作者：Administrator on 2016-09-27 14:22
+ * 邮箱：15818456427@163.com
+ */
+
+public class UserCenterActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
